d541308aeab24a9459f657657b9be3764155bf91
Khachatur <casilias35@hotmail.com>
2021-01-19 22:55:44 +0100

    form splitted

--- lib/connection_data.php
+++ lib/connection_data.php
@@ -1,9 +1,10 @@
 <?php
 
 $servername = "185.115.218.166";
+$dbname = "sofie21";
 $username = "sofie21";
 $password = "m141iiAJKHAwSv5I2Zup";
-$dbname = "sofie21";
+
 
 
 
--- lib/pdo.php
+++ lib/pdo.php
@@ -1,6 +1,5 @@
 <?php
 require_once "autoload.php";
-
 function CreateConnection()
 {
     global $conn;
