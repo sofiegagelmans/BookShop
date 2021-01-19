9717c525988b1fdd5c5e16aec656f0237e02fc67
Khachatur <casilias35@hotmail.com>
2021-01-20 00:41:28 +0100

    index pagina adding images from database

--- index.php
+++ index.php
@@ -14,9 +14,56 @@ PrintHeader();
 
 ?>
 <main class="main main-index">
+
     <?php
+
     PrintMain();
+    print '<div class="section-body">';
+
+//
+
+    $rows = GetData("select * from Product");
+
+//    //loop over de afbeeldingen
+    foreach ($rows as $row) {
+        $link_image = "images/" . $row['pro_image'];
+
+        //de kolom met de titel en de afbeelding erin
+
+
+        print '<div class="book-info">';
+        print '<div class="book-info-body">';
+        print '<div class="book-info-body-text">';
+        print '<a class="#" href="#">';
+        print '<img src="'.$link_image.'" alt="bookcover"/>';
+        print '</a>';
+        print '<div class="cart">';
+        print '<a href="#">Add To Cart</a>';
+        print '</div>';
+        print '<div class="more-info">';
+        print '<a href="./detail_book.php">More Info</a>';
+        print '</div>';
+        print '</div>';
+        print '</div>';
+        print '<div class="book-info-footer">';
+        print '<div class="book-title">' . $row['pro_name'] . '</div>';
+        print '<div class="book-price">' . $row['pro_price'] . '</div>';
+        print '<div class="book-stock">In Stock</div>';
+        print '</div>';
+        print '</div>';
+
+
+
+
+    }
+
+    print '</div>';
+
+
     ?>
+
+
+    </section>
 </main>
 
 
